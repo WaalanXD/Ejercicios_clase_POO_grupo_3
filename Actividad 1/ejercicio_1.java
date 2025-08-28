112a3e0c820bd63f7f42786c32c65ec51cd06f39
Kenny Daniel Diaz Diaz <kennydanieldiaz@gmail.com>
2025-08-28 15:47:15 -0500

    Implement age calculation for Juan, Alberto, Ana, and Mama
    Added functionality to calculate and display ages based on Juan's age input.

--- Actividad 1/ejercicio_1.java
+++ Actividad 1/ejercicio_1.java
@@ -1,1 +1,28 @@
-
+import java.util.Scanner;
+class ejercicio_1 {
+    public static void main(String[] args) {
+        
+    // Nombre de ejercicio: Ejercicio resuelto No 4
+    Scanner input = new Scanner(System.in);
+    System.out.println("Ingrese la edad de Juan: ");
+    int edadJuan = input.nextInt();
+    
+    Funciones.EvaluacionesEdad(edadJuan);
+    
+    int edadAlberto = Funciones.CalcularEdadAlberto(edadJuan);
+    
+    int edadAna = Funciones.CalcularEdadAna(edadJuan);
+    
+    int edadMama = Funciones.CalcularEdadMama(edadJuan,edadAlberto, edadAna);
+    
+    System.out.println("Las edades son:")
+    System.out.println("Alberto: " + edadAlberto);
+    System.out.println("Juan: ") + edadJuan;
+    System.out.println("Ana: ") + edadAna;
+    System.out.println("Mama: ") + edadMama;
+    
+    
+    
+    
+    
+}
