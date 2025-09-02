1fddf3a064dc411675e373611b35a80e393016e3
Waalan <kennydanieldiaz@gmail.com>
2025-09-02 12:40:40 -0500

    Add Calculos class and ejercicio_5 for circle calculations

---
+++ Actividad 1/Calculos.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+
+
+public class Calculos {
+    
+    
+    static double calcularLongitud(double radio) {
+
+
+
+    }
+
+
+    static double calcularArea(double radio) {
+
+
+        
+    }
+    
+}
---
+++ Actividad 1/ejercicio_5.java
@@ -0,0 +1,33 @@
+// EJERCICIO PROPUESTO 17
+
+import java.util.Scanner;
+
+
+public class ejercicio_5 {
+
+    public static void main(String[] args) {
+
+        double radio, longitudCircunferencia, areaCirculo;
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Ingrese el numero del radio");
+
+        radio = input.nextDouble();
+
+
+        longitudCircunferencia = Calculos.calcularLongitud(radio);
+
+        areaCirculo = Calculos.calcularArea(radio);
+
+        System.out.println("La longitud del Circulo es: " + longitudCircunferencia);
+
+        System.out.println("El area del circulo es: " + areaCirculo);
+        
+    }
+
+}
+
+git config --global user.name "Waalan"
+git config --global user.email "kennydanieldiaz@gmail.com"
