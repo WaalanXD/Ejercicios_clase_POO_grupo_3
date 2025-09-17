bd80d89bf706b78428d9458925b0392c8c5e588e
Kenny Daniel Diaz Diaz <kennydanieldiaz@gmail.com>
2025-09-16 20:16:52 -0500

    Refactor EjercicioN22 and fix Planeta usage

--- actividad02/ejercicio02/EjercicioN22.java
+++ actividad02/ejercicio02/EjercicioN22.java
@@ -1,55 +1,23 @@
+package actividad02.ejercicio02;
 
-
-public class Main {
+class EjercicioN22 {
     public static void main(String[] args) {
         
+        Planeta p1 = new Planeta("Tierra",1,5.9736E24,1.08321E12,12742,150000000,tipoPlaneta.TERRESTRE,true);
+
+        p1.imprimir();
+        
+        System.out.println("Densidad del planeta: "+p1.calcularDensidad());
+        
+        System.out.println("¿Es planeta exterior?: "+p1.esPlanetaEsterior());
+        
+        Planeta p2 = new Planeta(Planeta("Júpiter",79,1.899E27,1.4313E15,139820,750000000,tipoPlaneta.GASEOSO,true);
+        
+        p2.imprimir();
+        
+        System.out.println("Densidad del planeta: "+p2.calcularDensidad());
+        
+        System.out.println("¿Es planeta exterior?: "+p2.esPlanetaEsterior());
+    
     }
-}
-
-
-public class Planeta {
-    
-    String nombre = null;
-    int cantidadSatelites = 0;
-    double masa = 0;
-    double volumen = 0;
-    int diametro = 0;
-    int distanciaSol = 0;
-    boolean esObservable = false;
-    tipoPlaneta tipo = null;
-    
-    
-    Planeta(String nombre, int cantidadSatelites, double masa, double volumen, int diametro, int distanciaSol, tipoPlaneta tipo, boolean esObservable;) {
-        
-        this.nombre = nombre;
-        this.cantidadSatelites = cantidadSatelites;
-        this.masa = masa;
-        this.volumen = volumen;
-        this.diametro = diametro;
-        this.distanciaSol;
-        this.tipo = tipo;
-        this.esOberservable = esObersevable;
-        
-    }
-    void imprimir() {
-        
-        System.out.println(“Nombre del planeta = “ + nombre);
-    System.out.println(“Cantidad de satélites = “ + cantidadSatelites);
-    System.out.println(“Masa del planeta = “ + masa);
-    System.out.println(“Volumen del planeta = “ + volumen);
-    System.out.println(“Diámetro del planeta = “ + diametro);
-    System.out.println(“Distancia al sol = “ + distanciaSol);
-    System.out.println(“Tipo de planeta = “ + tipo);
-    System.out.println(“Es observable = “ + esObservable);
-    }
-        
-    
-
-}
-
-public enum tipoPlaneta {
-    
-    GASEOSO;
-    TERRESTRE;
-    ENANO;
 }
