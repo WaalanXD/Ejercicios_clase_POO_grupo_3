5270be2def684391d4b899805ed5f3c6f978d829
Kenny Daniel Diaz Diaz <kennydanieldiaz@gmail.com>
2025-09-18 17:25:34 +0000

    Corregir error tipográfico en la llamada al método esPlanetaExterior en EjercicioN22

--- actividad02/ejercicio02/EjercicioN22.java
+++ actividad02/ejercicio02/EjercicioN22.java
@@ -9,7 +9,7 @@ public class EjercicioN22 {
         
         System.out.println("Densidad del planeta: "+p1.calcularDensidad());
         
-        System.out.println("¿Es planeta exterior?: "+p1.esPlanetaEsterior());
+        System.out.println("¿Es planeta exterior?: "+p1.esPlanetaExterior());
         
         Planeta p2 = new Planeta("Júpiter",79,1.899E27,1.4313E15,139820,750000000,tipoPlaneta.GASEOSO,true);
         
@@ -17,7 +17,7 @@ public class EjercicioN22 {
         
         System.out.println("Densidad del planeta: "+p2.calcularDensidad());
         
-        System.out.println("¿Es planeta exterior?: "+p2.esPlanetaEsterior());
+        System.out.println("¿Es planeta exterior?: "+p2.esPlanetaExterior());
     
     }
 }
