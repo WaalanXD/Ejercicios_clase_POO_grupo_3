1c4f4954ad155a0c64fb789aff17463c29aad27d
Waalan <kennydanieldiaz@gmail.com>
2025-09-22 14:07:03 -0500

    Agregar clases Circulo y Ejercicio24 al paquete actividad02.ejercicio04

---
+++ actividad02/ejercicio04/Circulo.java
@@ -0,0 +1,11 @@
+package actividad02.ejercicio04;
+
+public class Circulo {
+    public int radio;
+
+
+    Circulo(int radio) {
+        this.radio = radio;
+        
+    }
+}
---
+++ actividad02/ejercicio04/Ejercicio24.java
@@ -0,0 +1,5 @@
+package actividad02.ejercicio04;
+
+public class Ejercicio24 {
+    
+}
