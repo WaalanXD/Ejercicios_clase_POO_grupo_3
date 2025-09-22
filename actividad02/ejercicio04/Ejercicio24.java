52272cc07918a2b0b4b946502d4dcac26887f201
Waalan <kennydanieldiaz@gmail.com>
2025-09-22 15:28:12 -0500

    Aumentar la longitud de las líneas divisorias en la salida del Ejercicio24

--- actividad02/ejercicio04/Ejercicio24.java
+++ actividad02/ejercicio04/Ejercicio24.java
@@ -14,13 +14,13 @@ public class Ejercicio24 {
 
     System.out.println("El area del circulo es: " + figura1.calcularArea());
     System.out.println("El perimetro del circulo es: " + figura1.calcularPerimetro());
-    System.out.println("-".repeat(30));
+    System.out.println("-".repeat(50));
     System.out.println("El area del cuadrado es: " + figura2.calcularArea());
     System.out.println("El perimetro del cuadrado es: " + figura2.calcularPerimetro());
-    System.out.println("-".repeat(30));
+    System.out.println("-".repeat(50));
     System.out.println("El area del rectangulo es: " + figura3.calcularArea());
     System.out.println("El perimetro del rectangulo es: " + figura3.calcularPerimetro());
-    System.out.println("-".repeat(30));
+    System.out.println("-".repeat(50));
     System.out.println("El area del triangulo es: " + figura4.calcularArea());
     System.out.println("La hipotenusa del triangulo es: " + figura4.calcularHipotenusa());
     System.out.println("El tipo de triangulo es: " + figura4.determinarTipoTriangulo());
