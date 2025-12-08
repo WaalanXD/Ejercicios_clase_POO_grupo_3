13a55ef4107e5d75d6c58bb52d7295ff6bc748c4
Waalan <kennydanieldiaz@gmail.com>
2025-12-08 14:56:06 -0500

    a

--- actividad06/ListaEmpleados.java
+++ actividad06/ListaEmpleados.java
@@ -1,8 +1,8 @@
 package actividad06;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Collections;
+import java.util.List;
 
 public class ListaEmpleados {
     private final List<Empleado> empleados;
